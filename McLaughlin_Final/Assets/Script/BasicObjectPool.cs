34f3567ace344629102ad757424d3c75277ffca0
McLaughlin <samclaughlin@labs.neit.edu>
2018-03-07 12:56:20 -0500

    3/7 updates

---
+++ McLaughlin_Final/Assets/Script/BasicObjectPool.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicObjectPool : MonoBehaviour {
+    public GameObject prefab;
+    private Stack<GameObject> inactiveInstances = new Stack<GameObject>();
+
+    public GameObject GetObj()
+    {
+        GameObject spawnObject;
+        if (inactiveInstances.Count > 0)
+        {
+            spawnObject = inactiveInstances.Pop();
+        }
+        else
+        {
+            spawnObject = Instantiate(prefab);
+
+            PooledObject pooledObject = spawnObject.AddComponent<PooledObject>();
+            pooledObject.pool = this;
+        }
+
+        spawnObject.SetActive(true);
+        return spawnObject;
+    }
+
+    public void ReturnObj(GameObject toReturn)
+    {
+        PooledObject pooledObject = toReturn.GetComponent<PooledObject>();
+
+        if (pooledObject != null && pooledObject.pool == this)
+        {
+            toReturn.SetActive(false);
+            inactiveInstances.Push(toReturn);
+        }
+        else
+        {
+            Destroy(toReturn);
+        }
+    }
+}
+
+public class PooledObject : MonoBehaviour
+{
+    public BasicObjectPool pool;
+}
